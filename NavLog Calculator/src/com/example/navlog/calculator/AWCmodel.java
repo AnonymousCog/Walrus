60b906ecb939c08e5cc6d40a4de9b5b1fe4f6412
Francisco Salinas <fssaez@gmail.com>
2013-04-21 22:49:58 -0400

    URL demo para captar los METARS de salida y llegada

--- NavLog Calculator/src/com/example/navlog/calculator/AWCmodel.java
+++ NavLog Calculator/src/com/example/navlog/calculator/AWCmodel.java
@@ -1,5 +1,37 @@
 package com.example.navlog.calculator;
 
-public class AWCmodel {
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
 
+
+public class AWCmodel 
+{
+	public void test()
+	{
+		try 
+		{
+			String uri =String.format("http://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&"+
+						"requestType=retrieve&format=xml&mostRecentForEachStation=constraint&hoursBeforeNow=5&stationString={0},{1}","TJSJ", "TJMZ");
+				    
+				URL url = new URL(uri);
+				HttpURLConnection connection =
+				    (HttpURLConnection) url.openConnection();
+				connection.setRequestMethod("GET");
+				connection.setRequestProperty("Accept", "application/xml");
+
+				InputStream xml = connection.getInputStream();
+
+				DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+				DocumentBuilder db = dbf.newDocumentBuilder();
+				Document doc = db.parse(xml);
+		} 
+		catch (Exception e) 
+		{
+            System.out.println("XML Passing Exception = " + e);
+        }
+	}
 }
